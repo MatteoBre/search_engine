ac9b84d0930d547f548277b719e2257038da5584
space_mushroom <matteo.bresciani.997@gmail.com>
2020-11-10 12:19:51 +0000

    now reading the dataset and creating a list of lucene documents

--- src/main/java/Indexer.java
+++ src/main/java/Indexer.java
@@ -1,6 +1,9 @@
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
 
 import java.io.*;
 import java.util.ArrayList;
@@ -16,14 +19,37 @@ public class Indexer {
     }
 
     private List<Document> getAllDocumentsFromCranfield() throws IOException {
+        List<CranfieldDocument> cranfieldDocuments = new ArrayList<>();
         List<Document> documents = new ArrayList<>();
         BufferedReader reader = new BufferedReader(new FileReader(new File(documentLocation)));
 
         String currentLine;
+        StringBuilder currentDoc = new StringBuilder();
+
         while((currentLine = reader.readLine()) != null) {
-            System.out.println(currentLine);
+            if(currentLine.startsWith(".I")) {
+                if(!currentDoc.toString().equals("")) {
+                    cranfieldDocuments.add(CranfieldDocument.fromString(currentDoc.toString()));
+                }
+
+                currentDoc.setLength(0);
+            }
+
+            currentDoc.append(currentLine).append("\n");
         }
 
+        // Adding the last document
+        if(!currentDoc.toString().equals("")) {
+            cranfieldDocuments.add(CranfieldDocument.fromString(currentDoc.toString()));
+        }
+
+        Document currentLuceneDocument;
+        for(CranfieldDocument doc : cranfieldDocuments) {
+            currentLuceneDocument = new Document();
+            currentLuceneDocument.add(new TextField("content", doc.getText(), Field.Store.YES));
+            documents.add(currentLuceneDocument);
+        }
+        
         return documents;
     }
 
