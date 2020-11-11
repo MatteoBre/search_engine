98e1e7089ade5b00e8db8a6571c20f063f2d939a
space_mushroom <matteo.bresciani.997@gmail.com>
2020-11-11 01:20:54 +0000

    sanitized the queries

--- src/main/java/QuerySolver.java
+++ src/main/java/QuerySolver.java
@@ -11,6 +11,26 @@ public class QuerySolver {
         this.indexLocation = indexLocation;
     }
 
+    // I use this function to remove everything that is not alphanumeric (and trim the result)
+    private String sanitize(String str) {
+        StringBuilder result = new StringBuilder();
+
+        char current;
+        for(int i=0; i<str.length(); i++) {
+            current = str.charAt(i);
+            if(!Character.isLetterOrDigit(current) && current != ' ') {
+                // Useful in cases like "word1(word2" -> "word1 word2"
+                // Without result.append(' '); it would be -> "word1word2", not what we want
+                result.append(' ');
+                continue;
+            }
+
+            result.append(current);
+        }
+
+        return result.toString().trim();
+    }
+
     private List<String> getQueries() throws IOException {
         List<String> queries = new ArrayList<>();
         BufferedReader reader = new BufferedReader(new FileReader(new File(queriesFileLocation)));
@@ -21,21 +41,24 @@ public class QuerySolver {
         while((currentLine = reader.readLine()) != null) {
             if(currentLine.startsWith(".I")) {
                 if(!currentQuery.toString().equals("")) {
-                    queries.add(currentQuery.toString());
+                    queries.add(sanitize(currentQuery.toString()));
                 }
 
                 currentQuery.setLength(0);
+                // I am skipping the lines with .I as they do not contain information
                 continue;
             }
-            if(currentLine.startsWith(".W"))
+            // I am skipping the lines with .W as they do not contain information
+            if(currentLine.startsWith(".W")) {
                 continue;
+            }
 
             currentQuery.append(currentLine).append("\n");
         }
 
         // Adding the last query
         if(!currentQuery.toString().equals("")) {
-            queries.add(currentQuery.toString());
+            queries.add(sanitize(currentQuery.toString()));
         }
 
         return queries;
